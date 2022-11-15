a00bd1bd14f3256b2b4da4b5e5ddec2770351e6c
VS-Sousa <vinicius.sousa@sptech.school>
2022-11-15 13:45:45 -0300

    Adicionando projeto individual MMT

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from psutil import cpu_percent, cpu_count, cpu_freq, virtual_memory, disk_usage, disk_io_counters, process_iter
+from psutil import cpu_percent, cpu_count, cpu_freq, virtual_memory, disk_usage, disk_io_counters, process_iter, net_connections, CONN_LISTEN
 import platform
 import getmac
 import mysql.connector
@@ -9,12 +9,12 @@ from dashing import HSplit, VSplit, Text
 from time import sleep
 import requests
 import json
+import datetime
 
 HOST = "localhost"
 USER = "aluno"
 PASS = "sptech"
 DB = "safecommerce"
-
 
 SLA_AVISO = 120
 SLA_EMERGENCIA = 60
@@ -106,7 +106,7 @@ def cadastrar_servidor():
     conexao = mysql.connector.connect(host=HOST, user=USER, password=PASS, database=DB)
     cursor = conexao.cursor()
 
-    cursor.execute(f"INSERT INTO Servidor VALUES (null, '{modelo}', '{so}', '{mac_add}', {fk_empresa})")
+    cursor.execute(f"INSERT INTO Servidor (modelo, so, mac_add, fkEmpresa) VALUES ('{modelo}', '{so}', '{mac_add}', {fk_empresa})")
     conexao.commit()
 
     if cursor.rowcount == 0:
@@ -159,7 +159,7 @@ def obter_parametros_coleta(id_servidor):
     conexao = mysql.connector.connect(host=HOST, user=USER, password=PASS, database=DB)
     cursor = conexao.cursor()
 
-    cursor.execute(f"SELECT fk_metrica FROM Parametro WHERE fk_servidor = {id_servidor}")
+    cursor.execute(f"SELECT fk_Metrica FROM Parametro WHERE fk_Servidor = {id_servidor}")
 
     parametros = cursor.fetchall()
 
@@ -174,10 +174,9 @@ def enviar_mensagem_slack(mensagem):
     
     # Variável que irá obter reposta que iremos receber da API. Logo depois do sinal de igual tem a chamada da bilioteca de solicitaçao. 
     # E também o link do bot criado para o envio de mensagens
-    resposta = requests.post('https://hooks.slack.com/services/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', data=payload)
+    requests.post('https://hooks.slack.com/services/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', data=payload)
 
 def abrir_issue_jira():
-
     url = "https://safe-commercefr.atlassian.net/rest/api/2/issue"
 
     headers={
@@ -208,6 +207,18 @@ def abrir_issue_jira():
 
     print(response.text)
 
+def obter_aplicacoes(id_servidor):
+    conexao = mysql.connector.connect(host=HOST, user=USER, password=PASS, database=DB)
+    cursor = conexao.cursor()
+
+    cursor.execute(f"SELECT nome, porta FROM Aplicacao WHERE fkServidor = {id_servidor}")
+
+    aplicacoes = cursor.fetchall()
+
+    cursor.close()
+    conexao.close() 
+
+    return aplicacoes
 
 def lidar_coleta_dados():
     interface = HSplit(
@@ -251,6 +262,7 @@ def lidar_coleta_dados():
 
     os.system(limpar)                
     while monitorando:
+        try:
             #Textos CPU
             CPU_L = interface.items[0].items[0]
             CPU_L.text = ''            
@@ -266,150 +278,193 @@ def lidar_coleta_dados():
             PROCESSOS = interface.items[2].items[0]
             PROCESSOS.text = ''
 
-            try:
-                parametros_coleta = obter_parametros_coleta(id_servidor)
+            parametros_coleta = obter_parametros_coleta(id_servidor)
 
-                leituras = []
+            leituras = []
 
-                for parametro in parametros_coleta:
-                    metrica = parametro[0]
+            for parametro in parametros_coleta:
+                metrica = parametro[0]
 
-                    if metrica == 1:
-                        # Porcentagem de uso da CPU (%)
+                if metrica == 1:
+                    # Porcentagem de uso da CPU (%)
 
-                        TIPO_SLA = "CPU"
+                    TIPO_SLA = "CPU"
 
-                        valor_lido = cpu_percent(interval=0.5)
-                        componente = "CPU"
-                        situacao = 'n'
-                        CPU_L.text += f'\nPorcentagem de uso: {valor_lido}%\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+                    valor_lido = cpu_percent(interval=0.5)
+                    componente = "CPU"
+                    situacao = 'n'
+                    CPU_L.text += f'\nPorcentagem de uso: {valor_lido}%\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
 
-                        if(cpu_percent >= 85 and cpu_percent < 95):
-                            enviar_mensagem_slack()
-                        elif(cpu_percent > 95):
-                            abrir_issue_jira()
+                    if(cpu_percent >= 85 and cpu_percent < 95):
+                        enviar_mensagem_slack()
+                    elif(cpu_percent > 95):
+                        abrir_issue_jira()
 
-                    elif metrica == 2:
-                        # Quatidade de CPU logica (vCPU)
+                elif metrica == 2:
+                    # Quatidade de CPU logica (vCPU)
 
-                        valor_lido = cpu_count(logical=True)
-                        componente = "vCPU"
-                        situacao = 'n'
-                        CPU_L.text += f'\nQuantidade de CPU lógica: {valor_lido}\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-                    
-                    elif metrica == 3:
-                        # Porcentagem de uso da CPU por CPU (%)
-
-                        coleta = cpu_percent(interval=0.5, percpu=True)
-
-                        for index in range(len(coleta)):
-                            valor_lido = coleta[index]
-                            componente = f"CPU {index + 1}"
-                            situacao = 'n'
-                            CPU_L.text += f'\nUso da {componente}: {valor_lido}%\n'
-                            leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                    elif metrica == 4:
-                        # Frequência de uso da CPU (MHz)
-
-                        valor_lido = cpu_freq().current
-                        componente = "CPU"
-                        situacao = 'n'
-                        CPU_L.text += f'\nFrequência de uso da CPU: {valor_lido}MHz\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                    elif metrica == 5:
-                        # Total de Memoria Ram (GB)
-
-                        valor_lido_bruto = virtual_memory().total
-                        valor_lido = transformar_bytes_em_gigas(valor_lido_bruto)
-                        componente = "RAM"
-                        situacao = 'n'
-                        RAM.text += f'\nTotal de memória RAM: {round(valor_lido)} GB\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                    elif metrica == 6: 
-                        # Porcentagem de uso da Memoria Ram (%)
-
-                        TIPO_SLA = "RAM"
-
-                        valor_lido = virtual_memory().percent
-                        componente = "RAM"
-                        situacao = 'n'
-                        RAM.text += f'\nTotal de uso de memória RAM: {valor_lido}%\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                        if(cpu_percent >= 85 and cpu_percent < 95):
-                            enviar_mensagem_slack()
-                        elif(cpu_percent > 95):
-                            abrir_issue_jira()
-
-                    elif metrica == 7:
-                        # Total de Disco (GB)
-
-                        valor_lido_bruto = disk_usage('/').total
-                        valor_lido = transformar_bytes_em_gigas(valor_lido_bruto)
-                        componente = "DISCO"
-                        situacao = 'n'
-                        DISCO.text += f'\nTotal de Disco: {round(valor_lido)} GB\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                    elif metrica == 8:
-                        # Porcentagem de uso de Disco (%)
-
-                        TIPO_SLA = "DISCO"
-
-                        valor_lido_bruto = disk_usage('/').percent
-                        componente = "DISCO"
-                        situacao = 'n'
-                        DISCO.text += f'\nTotal de uso de Disco: {valor_lido}%\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                        if(cpu_percent >= 85 and cpu_percent < 95):
-                            enviar_mensagem_slack()
-                        elif(cpu_percent > 95):
-                            abrir_issue_jira()
-
-                    elif metrica == 9:
-                        # Lido pelo Disco (ms)
-
-                        valor_lido = disk_io_counters('/').read_time
-                        componente = "DISCO"
-                        situacao = 'n'
-                        DISCO.text += f'\nTotal Lido Pelo Disco: {valor_lido} ms\n'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                    elif metrica == 10:
-                        # Escrito pelo Disco (ms)
-
-                        valor_lido = disk_io_counters('/').write_time
-                        componente = "DISCO"
-                        situacao = 'n'
-                        DISCO.text += f'\nTotal Escrito Pelo Disco: {valor_lido} ms'
-                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
-
-                #Listagem de Processos                
-                cont = 0                
-                for proc in process_iter(['pid', 'name', 'username']):
-                    if cont < 5:
-                        PROCESSOS.text += f'\nNome: {proc.name()}   Pid: {proc.pid} \n'
-
-                    cont += 1
-
-                if len(leituras) > 0 and controle_insert % 10 == 0:
-                    cursor.executemany("INSERT INTO Leitura VALUES (%s, %s, now(), %s, %s, %s)", leituras)
-                    conexao.commit()
-
-                    leituras.clear()
-
-                controle_insert += 1
-                interface.display()
-                sleep(0.5)
+                    valor_lido = cpu_count(logical=True)
+                    componente = "vCPU"
+                    situacao = 'n'
+                    CPU_L.text += f'\nQuantidade de CPU lógica: {valor_lido}\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
                 
-            except KeyboardInterrupt:
-                monitorando = False
+                elif metrica == 3:
+                    # Porcentagem de uso da CPU por CPU (%)
+
+                    coleta = cpu_percent(interval=0.5, percpu=True)
+
+                    for index in range(len(coleta)):
+                        valor_lido = coleta[index]
+                        componente = f"CPU {index + 1}"
+                        situacao = 'n'
+                        CPU_L.text += f'\nUso da {componente}: {valor_lido}%\n'
+                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 4:
+                    # Frequência de uso da CPU (MHz)
+
+                    valor_lido = cpu_freq().current
+                    componente = "CPU"
+                    situacao = 'n'
+                    CPU_L.text += f'\nFrequência de uso da CPU: {valor_lido}MHz\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 5:
+                    # Total de Memoria Ram (GB)
+
+                    valor_lido_bruto = virtual_memory().total
+                    valor_lido = transformar_bytes_em_gigas(valor_lido_bruto)
+                    componente = "RAM"
+                    situacao = 'n'
+                    RAM.text += f'\nTotal de memória RAM: {round(valor_lido)} GB\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 6: 
+                    # Porcentagem de uso da Memoria Ram (%)
+
+                    TIPO_SLA = "RAM"
+
+                    valor_lido = virtual_memory().percent
+                    componente = "RAM"
+                    situacao = 'n'
+                    RAM.text += f'\nTotal de uso de memória RAM: {valor_lido}%\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                    if(cpu_percent >= 85 and cpu_percent < 95):
+                        enviar_mensagem_slack()
+                    elif(cpu_percent > 95):
+                        abrir_issue_jira()
+
+                elif metrica == 7:
+                    # Total de Disco (GB)
+
+                    valor_lido_bruto = disk_usage('/').total
+                    valor_lido = transformar_bytes_em_gigas(valor_lido_bruto)
+                    componente = "DISCO"
+                    situacao = 'n'
+                    DISCO.text += f'\nTotal de Disco: {round(valor_lido)} GB\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 8:
+                    # Porcentagem de uso de Disco (%)
+
+                    TIPO_SLA = "DISCO"
+
+                    valor_lido_bruto = disk_usage('/').percent
+                    componente = "DISCO"
+                    situacao = 'n'
+                    DISCO.text += f'\nTotal de uso de Disco: {valor_lido}%\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                    if(cpu_percent >= 85 and cpu_percent < 95):
+                        enviar_mensagem_slack()
+                    elif(cpu_percent > 95):
+                        abrir_issue_jira()
+
+                elif metrica == 9:
+                    # Lido pelo Disco (ms)
+
+                    valor_lido = disk_io_counters('/').read_time
+                    componente = "DISCO"
+                    situacao = 'n'
+                    DISCO.text += f'\nTotal Lido Pelo Disco: {valor_lido} ms\n'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 10:
+                    # Escrito pelo Disco (ms)
+
+                    valor_lido = disk_io_counters('/').write_time
+                    componente = "DISCO"
+                    situacao = 'n'
+                    DISCO.text += f'\nTotal Escrito Pelo Disco: {valor_lido} ms'
+                    leituras.append((id_servidor, metrica, valor_lido, situacao, componente))
+
+                elif metrica == 11:
+                    # Temperatura da CPU
+                    print('TÁ PEGANDO FOGO BICHO')
+
+                elif metrica == 12:
+                    # Processos
+
+                    #Listagem de Processos
+                    cont = 0                
+                    for proc in process_iter(['pid', 'name', 'username']):
+                        if cont < 5:
+                            PROCESSOS.text += f'\nNome: {proc.name()}   Pid: {proc.pid} \n'
+
+                        cont += 1
+
+                elif metrica == 13:
+                    # Conexões ativas TCP
+                    aplicacoes = obter_aplicacoes(id_servidor)
+                    network = net_connections(kind='inet')
+
+                    for app in aplicacoes:
+                        valor_lido = 0
+                        situacao = 'n'
+                        componente = f'{app[0]}:{app[1]}'
+
+                        raddrs = set()
+                        already_listen = False
+
+                        for conn in network:
+                            raddr = ''
+                            if (len(conn.raddr) > 2):
+                                raddr = f'{conn.raddr.ip}:{conn.raddr.port}'
+
+                            if (
+                                conn.laddr.port == app[1]  
+                                and (conn.status != CONN_LISTEN 
+                                    or (conn.status == CONN_LISTEN 
+                                    and not already_listen)) 
+                                and raddrs.issuperset(set(raddr))                               
+                            ):
+                                if (raddr != ''):
+                                    raddrs.add(raddr)
+
+                                if (conn.status == CONN_LISTEN):
+                                    already_listen = True
+                                
+                                valor_lido += 1   
+
+                        leituras.append((id_servidor, metrica, valor_lido, situacao, componente))                    
+
+            if len(leituras) > 0 and controle_insert % 10 == 0:
+                horario = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')[:-5]
+
+                cursor.executemany("INSERT INTO Leitura VALUES (%s, %s, " + horario +", %s, %s, %s)", leituras)
+                conexao.commit()
+
+                leituras.clear()
+
+            controle_insert += 1
+            interface.display()
+            sleep(0.5)
+            
+        except KeyboardInterrupt:
+            monitorando = False
     
     cursor.close()
     conexao.close()
